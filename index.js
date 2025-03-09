ede3ccb3a5d130d28819826a85409f9bea4c0d32
monis_khan <monisykhan@gmail.com>
2025-03-09 17:22:35 +0530

    fixed typo mistak n

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import puppeteer from 'puppeteer';
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 
 const app = express();
 app.use(cors());
