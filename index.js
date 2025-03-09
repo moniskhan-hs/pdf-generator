ec16473666d2d04735f70122812d94c2eea4ab64
monis_khan <monisykhan@gmail.com>
2025-03-09 19:29:34 +0530

    added the timeout

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import express from "express";
+import cors from "cors";
+import puppeteer from "puppeteer-extra";
+import chromium from "@sparticuz/chromium";
 
+import  stealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(stealthPlugin());
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 4444;
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 chromium.setHeadlessMode = true;
 chromium.setGraphicsMode = false;
@@ -19,50 +21,55 @@ app.get("/generatePdf", async (req, res) => {
   try {
     const options = {
       args: chromium.args,
-      executablePath: isProduction 
+      executablePath: isProduction
         ? await chromium.executablePath()
-        : 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        : "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
       headless: chromium.headless,
-      defaultViewport: chromium.defaultViewport
+      defaultViewport: chromium.defaultViewport,
     };
 
     browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
     console.log("Navigating to URL:", req.query.url);
-    await page.goto(req.query.url, { waitUntil: "networkidle2" ,timeout:120000});
+    await page.goto(req.query.url, {
+      waitUntil: "networkidle2",
+      timeout: 120000,
+    });
 
     // Login handling
-    console.log("start login with crendentials.......")
+    console.log("start login with crendentials.......");
     await page.type("#loginform-username", "rahul_matharu@handysolver.com");
     await page.type("#loginform-password", "Handy@123");
 
     await Promise.all([
-      page.waitForNavigation({ waitUntil: "networkidle2" }),
-      page.click("[type=submit]")
+      page.waitForNavigation({ waitUntil: "networkidle2",timeout: 120000, }),
+      page.click("[type=submit]"),
     ]);
-console.log('login completed')
-console.log("Navigating with authentication:", req.query.url);
+    console.log("login completed");
+    console.log("Navigating with authentication:", req.query.url);
 
-    await page.goto(req.query.url, { waitUntil: "networkidle2",timeout:120000});
-   
-console.log("Navigating with authentication completed:", req.query.url);
-const pdfBuffer = await page.pdf({ printBackground: true });
+    await page.goto(req.query.url, {
+      waitUntil: "networkidle2",
+      timeout: 120000,
+    });
+
+    console.log("Navigating with authentication completed:", req.query.url);
+    const pdfBuffer = await page.pdf({ printBackground: true });
 
     await browser.close();
 
     res.status(200).json({
       type: "pdf",
-      success: 'pdf generated successfully'
+      success: "pdf generated successfully",
     });
-    console.log('pdf genereted successfully')
-
+    console.log("pdf genereted successfully");
   } catch (error) {
     console.error("Error:", error);
     if (browser) await browser.close();
     res.status(500).json({
       message: "Failed to generate PDF",
-      error: error.message
+      error: error.message,
     });
   }
 });
@@ -70,11 +77,8 @@ const pdfBuffer = await page.pdf({ printBackground: true });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.get("/myname", async(req,res)=>{
-
-
-    res.status(200).json({
-        result: `here is query value ${req.query.url}`
-      });
-})
-
+app.get("/myname", async (req, res) => {
+  res.status(200).json({
+    result: `here is query value ${req.query.url}`,
+  });
+});
